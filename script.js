ea4c7cb9dcddebeaa5cf275a92c97aa7a695ae91
Arturo <jarsmdza@gmail.com>
2022-09-06 00:11:34 -0500

    Added Functionality to go for any color to black, found a bug any color goes to dark immediately

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let rangeValue = 4;
 let state = ``;
 let colors =[`#fff`,`#000`];
 
-colors=formEvents(); //checar si debe devolver valor
+colors=formEvents();
 createDivs();
 painting();
+let a = hexToRgb(`#b141fb`);
+console.log(a)
 
+colors[1].to
 function formEvents () {
     const formAction = document.querySelector(`.aside__form`);
     const background = document.querySelector(`.div-container`);
@@ -17,11 +20,14 @@ function formEvents () {
             e.preventDefault();
             switch (e.target.className) {
                 case `random`:
-                    state = `random`
+                    state = `random`;
                 break;
                 case `clc`:
                     background.innerHTML = "";
                     createDivs();
+                break;
+                case `dark`:
+                    state = `dark`;
                 break;
             }
         }
@@ -86,9 +92,49 @@ function painting() {
                 colorHex[2] = Math.floor(Math.random()*255);
                 e.target.style.backgroundColor = `rgb(${colorHex[0]},${colorHex[1]},${colorHex[2]})`;
             }
+            else if (state === `dark`) {
+                let rgbColor = []
+                if (e.target.style.backgroundColor.includes === `#`) {
+                    switch (e.target.style.backgroundColor) {
+                        case ``:
+                            rgbColor =hexToRgb(`#ffffff`);
+                        break;
+                        default:
+                            rgbColor = hexToRgb(`${e.target.style.backgroundColor}`);
+                        break;
+                    }
+                }
+                else if (e.target.style.backgroundColor.includes === `rgb`){
+                    console.log(`ejecuto rgb`)
+                    rgbColor = e.target.style.backgroundColor;
+                    rgbColor=rgbColor.split(`rgb`, 2);
+                    rgbColor =rgbColor[1].split (`,`, 3);
+                    console.log(rgbColor)
+                    rgbColor =[rgbColor[0] -= 17, rgbColor[1] -= 17, rgbColor[3] -= 17]
+                    if (rgbColor[0] < 0 || rgbColor[1] < 0 || rgbColor[2] < 0) {
+                        rgbColor = [0,0,0];
+                    }
+                    e.target.style.backgroundColor= `rgb(${rgbColor[0]},${rgbColor[1]},${rgbColor[2]})`;
+                }
+                else {
+                    console.log(e.target.style.backgroundColor)
+                }
+                e.target.style.backgroundColor= `rgb(${rgbColor[0]},${rgbColor[1]},${rgbColor[2]})`;
+            }
+
             else {
                 e.target.style.backgroundColor = `${colors[1]}`;
             }
         }
     });
+}
+
+function hexToRgb(hexColor) {
+    hexColor = hexColor.split(`#`,2);
+    console.log(hexColor[1])
+    let rgbString = [];
+    for (let i = 0; i < 6; i += 2) {
+        rgbString.push(parseInt(`${hexColor[1][i]}${hexColor[1][i+1]}`,16));
+    }
+    return  rgbString;
 }
